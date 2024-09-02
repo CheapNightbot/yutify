56ac45f3f32a0c8ce600f617e57a6e9408102815
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2024-09-02 18:17:04 +0900

    📑 Add `/yutify` & `/docs` route
    FINALLY ~ !!!!!!!!!!!! <。)#)))≦

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 import os
 import re
 
-from flask import Flask, jsonify, make_response, render_template
+import requests
+from flask import Flask, jsonify, make_response, render_template, request
+from flask_caching import Cache
 from flask_cors import CORS
 from flask_limiter import Limiter, RequestLimit
 from flask_limiter.util import get_remote_address
@@ -15,6 +17,7 @@ redis_uri = os.environ["REDIS_URI"]
 
 app = Flask(__name__)
 api = Api(app)
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
@@ -28,13 +31,14 @@ limiter = Limiter(
     key_func=get_remote_address,
     app=app,
     storage_uri=redis_uri,
-    strategy="fixed-window",
+    strategy="fixed-window-elastic-expiry",
     on_breach=default_error_responder,
 )
 
 
 class Yutify(Resource):
     @limiter.limit("60 per minute")
+    @cache.cached(timeout=3600)
     def get(self, artist, song):
         ytmusic = musicyt.search_musicyt(artist, song)
         spotify = spotipy.search_music(artist, song)
@@ -74,5 +78,51 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/yutify")
+def yutify_me():
+    artist = request.args.get("artist")
+    song = request.args.get("song")
+
+    url = f"https://yutify.onrender.com/api/{artist}:{song}"
+    response = requests.get(url)
+    yutify = response.json()
+
+    if response.status_code == 404:
+        return render_template(
+            "index.html",
+            artist=artist,
+            song=song,
+            album_art="{{ url_for('static', filename='favicon.svg') }}",
+            title=f"{yutify['error']}",
+        )
+
+    album_art = yutify["album_art"]
+    title = yutify["title"]
+    album_type = yutify["album_type"]
+    album_title = yutify["album_title"]
+    artists = yutify["artists"]
+    spotify = yutify["spotify"]
+    yt_music = yutify["ytmusic"]["url"]
+
+    return render_template(
+        "index.html",
+        artist=artist,
+        song=song,
+        album_art=f"{album_art}",
+        title=f"{title}",
+        album_type=f"{album_type}",
+        album_title=f"{album_title}",
+        artists=f"{artists}",
+        spotipy=f"{spotify}",
+        yt_music=f"{yt_music}",
+    )
+
+
+@app.route("/docs")
+def docs():
+    return render_template("docs.html")
+
+
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    # serve(app, host="0.0.0.0", port=8000)
+    app.run(debug=True)
