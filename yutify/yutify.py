761041119611f6a987cbe1c662c228841435a0d8
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2024-10-11 06:48:20 +0900

    Update yutify.py

--- yutify/yutify.py
+++ yutify/yutify.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.logger import logger
+from utils.cheap_utils import cheap_compare
 from yutify.deezer import Deezer
 from yutify.itunes import Itunes
 from yutify.musicyt import MusicYT
@@ -14,19 +15,33 @@ spotipy = Spotipy(client_id, client_secret)
 yt_music = MusicYT()
 itunes = Itunes()
 
+# Global Variable
+priority = None
 
-def build_result(ytmusic_data=None, deezer_data=None, spotify_data=None, itunes_result=None):
+def build_result(
+    ytmusic_data=None, deezer_data=None, spotify_data=None, itunes_result=None
+):
     """Construct the final result using available data from YouTube Music, Deezer, and Spotify."""
+    global priority
+    match priority:
+        case "spotify":
+            result = spotify_data
+        case "deezer":
+            result = deezer_data
+        case "itunes":
+            result = ytmusic_data
+        case _:
+            result = ytmusic_data
     return {
-        "album_art": (spotify_data or deezer_data or ytmusic_data).get("album_art"),
-        "album_type": (spotify_data or ytmusic_data or deezer_data).get("album_type"),
-        "album_title": (spotify_data or deezer_data or ytmusic_data).get("album_title"),
-        "artists": (spotify_data or deezer_data or ytmusic_data).get("artists"),
+        "album_art": result.get("album_art"),
+        "album_type": result.get("album_type"),
+        "album_title": result.get("album_title"),
+        "artists": result.get("artists"),
         "deezer": deezer_data.get("url") if deezer_data else None,
         "itunes": itunes_result.get("url") if itunes_result else None,
         "release_date": (spotify_data or deezer_data or {}).get("release_date"),
         "spotify": spotify_data.get("url") if spotify_data else None,
-        "title": (spotify_data or deezer_data or ytmusic_data).get("title"),
+        "title": result.get("title"),
         "ytmusic": (
             {
                 "id": ytmusic_data.get("id"),
@@ -58,21 +73,30 @@ def get_itunes_result(artist: str, song: str):
     return result
 
 
-def get_spotify_result(artist: str, song: str, deezer_data=None):
-    """Search for the song in Spotify using Deezer data or fallback to user input."""
-    if deezer_data:
-        logger.info("Search Spotify with Deezer results.")
-        result = spotipy.search_advance(
-            deezer_data["artists"],
-            deezer_data["title"],
-            isrc=deezer_data.get("isrc"),
-            upc=deezer_data.get("upc"),
-        )
-    else:
-        logger.info("Search Spotify with user-provided data.")
-        result = spotipy.search_music(artist, song)
+def get_spotify_result(artist: str, song: str, deezer_data=None, itunes_data=None, ytmusic_data=None):
+    global priority
+    result = None
+    if deezer_data or itunes_data or ytmusic_data:
+        if cheap_compare(deezer_data["title"], song):
+            priority = "deezer"
+            logger.info("Search Spotify with Deezer results.")
+            result = spotipy.search_advance(
+                deezer_data["artists"],
+                deezer_data["title"],
+                isrc=deezer_data.get("isrc"),
+                upc=deezer_data.get("upc"),
+            )
+        elif cheap_compare(itunes_data["title"], song):
+            logger.info("Search Spotify with iTunes results.")
+            result = spotipy.search_music(itunes_data["artists"], itunes_data["title"])
+
+        else:
+            priority = "ytmusic"
+            logger.info("Search Spotify with YouTube Music results.")
+            result = spotipy.search_music(ytmusic_data["title"], ytmusic_data["artists"])
 
     if result:
+        priority = "spotify"
         logger.info("Got result from Spotify.")
     else:
         logger.info("Search Spotify with user-provided data.")
@@ -98,8 +122,14 @@ def yutify_it(artist: str, song: str):
     """
     deezer_result = get_deezer_result(artist, song)
     itunes_result = get_itunes_result(artist, song)
-    spotify_result = get_spotify_result(artist, song, deezer_data=deezer_result)
     ytmusic_result = get_ytmusic_result(artist, song)
+    spotify_result = get_spotify_result(
+        artist,
+        song,
+        deezer_data=deezer_result,
+        itunes_data=itunes_result,
+        ytmusic_data=ytmusic_result,
+    )
 
     # If no results found on any platform, return None
     if not (ytmusic_result or deezer_result or spotify_result or itunes_result):
