0e52e63fe5f5053f5071e5c84a39a29dee700b1d
cheapnightbot <cheapnightbot@gmail.com>
2025-02-08 05:01:59 +0000

    Add custom `404` page

--- api.py
+++ api.py
@@ -177,5 +177,10 @@ def docs():
     return render_template("docs.html", active_page="docs", year=date.today().year)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", error=error), 404
+
+
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
