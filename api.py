b95af977cf55d8b1060f483dfc4a8f9c362cfbe5
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-02-08 14:51:03 +0900

    Add custom `500` page & fix TypeError in lyrics

--- api.py
+++ api.py
@@ -87,7 +87,7 @@ def build_response_template(response, artist, song):
             deezer=yutify_data.get("deezer"),
             genre=yutify_data.get("genre"),
             itunes=yutify_data.get("itunes"),
-            lyrics=yutify_data.get("lyrics").replace("\r", "").replace("\n", "<br>"),
+            lyrics=yutify_data.get("lyrics", "").replace("\r", "").replace("\n", "<br>"),
             song=song,
             spotify=yutify_data.get("spotify"),
             title=yutify_data.get("title"),
@@ -179,7 +179,12 @@ def docs():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html", error=error), 404
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
