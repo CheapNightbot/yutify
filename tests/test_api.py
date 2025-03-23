b2c4344ff14fb2b9a86f2ac6f363baad922fe51c
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-03-24 07:04:56 +0900

    Generate Initial Migration Repository

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,17 @@
 import pytest
-from api import app
+from app import create_app, db
 
 
 # Fixture to provide a Flask test client
 @pytest.fixture
 def client():
+    app = create_app("config.TestingConfig")
     with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
         yield client
+        with app.app_context():
+            db.drop_all()
 
 
 def test_index(client):
