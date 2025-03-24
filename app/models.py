1d91e585693f929112ad81d33b13756eca8812d8
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-03-25 00:52:28 +0900

    Add UserService Model

--- app/models.py
+++ app/models.py
@@ -39,5 +39,17 @@ class User(Base):
 class Service(Base):
     __tablename__ = "services"
     service_id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    service_name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    service_name: so.Mapped[str] = so.mapped_column(
+        sa.String(64), index=True, unique=True
+    )
     service_url: so.Mapped[Optional[str]] = so.mapped_column(sa.String(64))
+
+
+class UserService(Base):
+    __tablename__ = "user_services"
+    user_services_id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.user_id), index=True)
+    service_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Service.service_id), index=True)
+    access_token: so.Mapped[str] = so.mapped_column(sa.String())
+    refresh_token: so.Mapped[str] = so.mapped_column(sa.String(), nullable=True)
+    expires_in: so.Mapped[int] = so.mapped_column(nullable=True)
