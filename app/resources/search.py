bd4265d07c1ec7842a8d5dfb97ad4c35ee1b3fac
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-04-27 16:44:35 +0900

    Save Access Token Info for Spotify & KKBox
    - for client credentials grant type/flow only.

--- app/resources/search.py
+++ app/resources/search.py
@@ -1,21 +1,93 @@
 import logging
 import os
 from dataclasses import asdict
+from typing import Union
 
+import sqlalchemy as sa
 from flask import request
 from flask_restful import Resource
-from yutipy import deezer, itunes, kkbox, musicyt, spotify, yutipy_music
+from yutipy import deezer, itunes, musicyt, yutipy_music
 from yutipy.exceptions import KKBoxException, SpotifyException
+from yutipy.kkbox import KKBox
 from yutipy.logger import enable_logging
+from yutipy.spotify import Spotify
 
-from app import cache
+from app import cache, db
 from app.limiter import limiter
+from app.models import Service
 
 # Create a logger for this module
 logger = logging.getLogger(__name__)
 enable_logging(handler=logger)
 
 RATELIMIT = os.environ.get("RATELIMIT")
+
+
+class BaseOverride:
+    """Custom Base class to override the `save_access_token` and `load_access_token` methods.
+
+    Only works for Client Credential grant type/flow.
+    """
+
+    SERVICE_NAME: str = None
+    SERVICE_URL: str = None
+    IS_PRIVATE: bool = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def save_access_token(self, token_info: dict) -> None:
+        service = db.session.scalar(
+            sa.select(Service).where(
+                Service.service_name.ilike(self.SERVICE_NAME.lower())
+            )
+        )
+        if not service:
+            service = Service(
+                service_name=self.SERVICE_NAME,
+                service_url=self.SERVICE_URL,
+                is_private=self.IS_PRIVATE,
+            )
+            db.session.add(service)  # Add the new service to the session
+
+        # Set the access token values
+        service.access_token = token_info.get("access_token")
+        service.expires_in = token_info.get("expires_in")
+        service.requested_at = token_info.get("requested_at")
+
+        # Commit the changes to the database
+        db.session.commit()
+
+    def load_access_token(self) -> Union[dict, None]:
+        service = db.session.scalar(
+            sa.select(Service).where(
+                Service.service_name.ilike(self.SERVICE_NAME.lower())
+            )
+        )
+
+        if service:
+            return {
+                "access_token": service.access_token,
+                "expires_in": service.expires_in,
+                "requested_at": service.requested_at,
+            }
+
+        return None
+
+
+class MySpotify(BaseOverride, Spotify):
+    """Custom Spotify class to override the `save_access_token` and `load_access_token` methods."""
+
+    SERVICE_NAME = "Spotify"
+    SERVICE_URL = "https://open.spotify.com"
+
+
+class MyKKBox(BaseOverride, KKBox):
+    """Custom KKBox class to override the `save_access_token` and `load_access_token` methods."""
+
+    SERVICE_NAME = "KKBox"
+    SERVICE_URL = "https://www.kkbox.com"
+    IS_PRIVATE = True
 
 
 class YutifySearch(Resource):
@@ -58,7 +130,7 @@ class YutifySearch(Resource):
                     result = apple_music.search(artist, song)
             case "kkbox":
                 try:
-                    with kkbox.KKBox() as kkbox_music:
+                    with MyKKBox() as kkbox_music:
                         result = kkbox_music.search(artist, song)
                 except KKBoxException as e:
                     logger.warning(
@@ -67,7 +139,7 @@ class YutifySearch(Resource):
                     result = None
             case "spotify":
                 try:
-                    with spotify.Spotify() as spotify_music:
+                    with MySpotify() as spotify_music:
                         result = spotify_music.search(artist, song)
                 except SpotifyException as e:
                     logger.warning(
@@ -79,6 +151,11 @@ class YutifySearch(Resource):
                     result = youtube_music.search(artist, song)
             case _:
                 with yutipy_music.YutipyMusic() as py_music:
+                    py_music.services["kkbox"] = MyKKBox()
+                    py_music.services["spotify"] = MySpotify()
+                    logger.debug(
+                        f"Custom services set: {list(py_music.services.values())}"
+                    )
                     result = py_music.search(artist, song)
 
         if not result:
