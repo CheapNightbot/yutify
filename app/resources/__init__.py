648093d0df6139a8c416949eb047ae2019522490
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-03 18:45:57 +0900

    Update __init__.py

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -6,6 +6,7 @@ from app.resources.user_activity import UserActivityResource
 
 bp = Blueprint("api", __name__)
 api = Api(bp)
+api.catch_all_404s = True
 
 api.add_resource(YutifySearch, "/search/<path:artist>:<path:song>")
 api.add_resource(UserActivityResource, "/me")
