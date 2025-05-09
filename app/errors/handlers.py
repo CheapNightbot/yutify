cc71d732b1e73ab4b1e6876104c9b0165f3f11e2
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-10 01:30:07 +0900

    Update handlers.py

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -6,14 +6,14 @@ from app.errors import bp
 
 @bp.app_errorhandler(404)
 def page_not_found(error):
-    return render_template("errors/404.html"), 404
+    return render_template("errors/404.html", error=error), 404
 
 @bp.app_errorhandler(403)
 def page_not_found(error):
-    return render_template("errors/403.html"), 403
+    return render_template("errors/403.html", error=error), 403
 
 
 @bp.app_errorhandler(500)
 def internal_server_error(error):
     db.session.rollback()
-    return render_template("errors/500.html"), 500
+    return render_template("errors/500.html", error=error), 500
