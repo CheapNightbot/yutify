934825d2e705ea59cf7b62dc077ba3944a25f19d
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-12 10:16:47 +0900

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,26 +1,7 @@
-import sqlalchemy as sa
 from flask_security import RegisterFormV2
-
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-
-from app import db
-from app.models import User
-
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 
 class RegistrationForm(RegisterFormV2):
     name = StringField("Name", validators=[DataRequired()])
-
-
-# class ResetPasswordRequestForm(FlaskForm):
-#     email = StringField("Email", validators=[DataRequired(), Email()])
-#     submit = SubmitField("Request Password Reset")
-
-
-# class ResetPasswordForm(FlaskForm):
-#     password = PasswordField("New Password", validators=[DataRequired()])
-#     password2 = PasswordField(
-#         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
-#     )
-#     submit = SubmitField("Reset Password")
