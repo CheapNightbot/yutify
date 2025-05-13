c3b36829e05919d9329f93caf7f9b8b6a8c18618
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-13 13:03:57 +0900

    Add TOS & Privacy Agreement checkbox

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_security import RegisterFormV2
-from wtforms import StringField
+from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class RegistrationForm(RegisterFormV2):
     name = StringField("Name", validators=[DataRequired()])
+    agreement = BooleanField("Agreement", validators=[DataRequired()])
