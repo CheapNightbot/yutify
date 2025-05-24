3ddf5568497e9aa859f233592a9c77d38435d4e6
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-24 13:42:06 +0900

    Conditionally Add Captcha

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,14 +1,22 @@
-from flask_security import RegisterFormV2, LoginForm
+import os
+
+from dotenv import load_dotenv
+from flask_security import LoginForm, RegisterFormV2
 from flask_wtf import RecaptchaField
-from wtforms import StringField, BooleanField
+from wtforms import BooleanField, StringField
 from wtforms.validators import DataRequired
+
+load_dotenv()
+ENABLE_CAPTCHA = bool(int(os.getenv("ENABLE_CAPTCHA", False)))
 
 
 class RegistrationForm(RegisterFormV2):
     name = StringField("Name", validators=[DataRequired()])
     agreement = BooleanField("Agreement", validators=[DataRequired()])
-    recaptcha = RecaptchaField()
+    if ENABLE_CAPTCHA:
+        recaptcha = RecaptchaField()
 
 
 class MyLoginForm(LoginForm):
-    recaptcha = RecaptchaField()
+    if ENABLE_CAPTCHA:
+        recaptcha = RecaptchaField()
