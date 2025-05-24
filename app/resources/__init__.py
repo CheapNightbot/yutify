91b9c937fe403fe9a89c521b662dc57080915870
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-25 01:08:12 +0900

    Update __init__.py

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -3,9 +3,11 @@ from flask_restful import Api
 
 from app.resources.search import YutifySearch
 from app.resources.activity import UserActivityResource
+from app.extensions import csrf
 
 bp = Blueprint("api", __name__)
 api = Api(bp)
+csrf.exempt(bp)
 
 api.add_resource(YutifySearch, "/search/<path:artist>:<path:song>")
 api.add_resource(UserActivityResource, "/me")
