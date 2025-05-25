ec6abeba6f09da874f1b6cba57752bec4140318f
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-25 17:56:57 +0900

    Update models.py
    - the `revoked` instance variable does not exist. me copied from the docs example and i believe i was there for explanation.. the `is_revoked()` method does what we want.

--- app/models.py
+++ app/models.py
@@ -294,7 +294,7 @@ class OAuth2Token(db.Model, OAuth2TokenMixin):
     user: so.Mapped["User"] = db.relationship('User')
 
     def is_refresh_token_active(self):
-        if self.revoked:
+        if self.is_revoked():
             return False
         expires_at = self.issued_at + self.expires_in * 2
         return expires_at >= time()
