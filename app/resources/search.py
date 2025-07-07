ce08a0cc89dbca94a7a0c6b92860f1e0f5831c73
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-07 19:05:55 +0900

    Set Search Result limit to 5

--- app/resources/search.py
+++ app/resources/search.py
@@ -204,7 +204,7 @@ class YutifySearch(Resource):
                 result = asdict(ALL)
             return OrderedDict(sorted(result.items())), 200
 
-        RESULT_LIMIT = 10
+        RESULT_LIMIT = 5
         match platform:
             case "deezer":
                 with deezer.Deezer() as deezer_music:
